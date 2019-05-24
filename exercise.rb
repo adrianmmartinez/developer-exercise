1c96155125e502624acfa1969211af65783a32fa
Adrian Martinez <adrianmartinez@MacBookAir.local>
2019-05-23 20:36:23 -0700

    edited with challenge answers

--- exercise.rb
+++ exercise.rb
@@ -1,18 +1,63 @@
 class Exercise
 
-  # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+
+    x = str.split(' ')
+    str_a = ''
+    last = '' 
+    temp = ''
+
+    x.each do | i |
+
+      last = '?' if i.end_with?('?')
+      last = '.' if i.end_with?('.')
+
+      if i.length <= 4
+        temp = i
+      elsif i =~ /[A-Z]./
+        temp = 'Marklar'
+      else
+        temp = 'marklar'
+      end
+      
+      temp << last + ' '
+      str_a << temp
+      last = ''
+      temp = ''
+
+    end
+
+    str_a.chomp(' ')
+
   end
 
-  # Return the sum of all even numbers in the Fibonacci sequence, up to
-  # the "nth" term in the sequence
-  # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
-  # and the sum of its even numbers is (2 + 8) = 10
+  # sums the even values up to the nth value of the fibonacci sequence
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+
+    idx = 1
+    sum = 0
+
+    while idx < nth
+
+      sum += fibonacci(idx) if fibonacci(idx).even?
+      idx += 1
+
+    end
+
+    sum
+
   end
 
+  # gets fibonacci value of given index
+  def self.fibonacci(n)
+
+    if n == 1 or n == 2
+      1
+    else 
+      fibonacci(n - 1) + fibonacci(n - 2)
+    end
+    
+  end
 end
